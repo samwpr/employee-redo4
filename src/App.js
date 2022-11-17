f694b86069eb388609f3a2da7c334deed6f71437
samwpr <swpr10x@gmail.com>
2022-11-17 17:10:13 +0800

    worked on map()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee.js';
+import {useState} from 'react';
 
 function App() {
+  const showEmployee = true;
+  const [role, setRole] = useState("Dev");
+  const [employee, setEmployee] = useState(
+    [
+    {name: "Sam", role: "Dev", img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"},
+    {name: "Sam", role: "Dev", img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"},
+    {name: "Sam", role: "Dev", img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"},
+    {name: "Sam", role: "Dev", img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"},
+    {name: "Sam", role: "Dev", img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"},
+    {name: "Sam", role: "Dev", img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"}
+    ]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      {showEmployee ? (
+        <div className="flex flex-wrap justify-center">
+          {employee.map((e) => {
+            return(
+            <Employee name={e.name} role={e.role} img={e.img}/>
+            )
+            })};
+
+        </div>
+      ) : <p>No Acesss</p>}
+     
     </div>
   );
 }
